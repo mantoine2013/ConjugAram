9ae9253ddaa97cd7009712a5346c65b550468638
mantoine2013 <michelantoine@hotmail.com>
2026-02-07 09:31:26 +0100

    Commit initial de l'application

--- src/Conjugueur/V/JFrameConjugRu.java
+++ src/Conjugueur/V/JFrameConjugRu.java
@@ -4,7 +4,13 @@
  */
 package Conjugueur.V;
 
+import static Conjugueur.V.JFrameConjug.FRANÇAIS;
+import static Conjugueur.V.JFrameConjug.NOMBRE;
+import static Conjugueur.V.JFrameConjug.jPanelDroit;
+import static Conjugueur.V.JFrameConjug.jPanelNb;
+import java.awt.ComponentOrientation;
 import java.awt.event.ActionListener;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
 
 public class JFrameConjugRu  extends JFrameConjDouble implements ActionListener {
@@ -18,9 +24,11 @@ public class JFrameConjugRu  extends JFrameConjDouble implements ActionListener 
         if (DEBUG) { System.out.println("Conjugueur.V.JFrameConjugRu::JFrameConjugRu, contrôleur.toString() ="+contrôleur.toString()) ; }
         frame.setTitle("Conjugueur russe V0.8"); 
         largeur = LARGEUR ; hauteur = HAUTEUR ;
-        jCBAutMot = new JComboBox(Conjugueur.M.ConjugAnFr.cbAutreMotM);
+        jCBAutMot = new JComboBox(Conjugueur.M.ConjugRu.cbAutreMotM);
+        jPanelNb.add(jComboBoxNb) ;     jPanelDroit.add(jPanelNb, java.awt.BorderLayout.EAST);    jComboBoxNb.setActionCommand(NOMBRE) ; jComboBoxNb.addActionListener(this); jCBAutMot.setRenderer(new ComboBoxVerbeRenderer(FRANÇAIS, ComponentOrientation.LEFT_TO_RIGHT, Conjugueur.M.ConjugRu.ATTRIBUT)); 
     
     }                                                                           // JFrameConjugRu
     public static final int HAUTEUR = 380, LARGEUR = 870 ;
+    private final  JComboBox<Conjugueur.M.ConjugRu.N> jComboBoxNb = new JComboBox<>(new DefaultComboBoxModel<>(Conjugueur.M.ConjugRu.N.values())) ;
     private final boolean DEBUG = true;        
 }
